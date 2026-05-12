15e1a47e4483ea77aa6d9c347bca2b74325c87b0
Zach Bienstock <zbienstock@gmail.com>
2026-05-12 09:27:45 -0400

    feat: add /dashboard/sticker-studio route

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ const Reports = lazy(() => import('./pages/Reports'))
 const Onboarding = lazy(() => import('./pages/Onboarding'))
 const ConversationSearch = lazy(() => import('./pages/ConversationSearch'))
 const MediaAgent = lazy(() => import('./pages/MediaAgent'))
+const StickerStudio = lazy(() => import('./pages/StickerStudio'))
 const CommunityHub = lazy(() => import('./pages/CommunityHub'))
 const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
 const ResetPassword = lazy(() => import('./pages/ResetPassword'))
@@ -87,6 +88,7 @@ function App() {
           <Route path="/onboarding" element={<PrivateRoute><Onboarding /></PrivateRoute>} />
           <Route path="/dashboard/search" element={<PrivateRoute><ConversationSearch /></PrivateRoute>} />
           <Route path="/dashboard/media" element={<PrivateRoute><MediaAgent /></PrivateRoute>} />
+          <Route path="/dashboard/sticker-studio" element={<PrivateRoute><StickerStudio /></PrivateRoute>} />
           <Route path="/dashboard/community" element={<PrivateRoute><CommunityHub /></PrivateRoute>} />
 
           <Route path="/admin" element={<Admin />} />
